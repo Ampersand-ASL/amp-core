384d1f17fd60a859cb73bc10eb712b8911992631
Bruce MacKinnon <bruce@mackinnon.com>
2025-12-15 12:04:00 -0500

    Bridge mixing unit test works

--- src/tests/bridge-test-1.cpp
+++ src/tests/bridge-test-1.cpp
@@ -8,18 +8,31 @@
 using namespace std;
 using namespace kc1fsz;
 
-
 class TestSink : public MessageConsumer {
 public:
 
     void consume(const Message& msg) {
-        cout << "Dest line/call " << msg.getDestBusId() << "/" << msg.getDestCallId() << endl;
-        int16_t* r = (int16_t*)msg.body();
-        cout << "  DC " << r[0] << endl;
+        //cout << "Dest line/call " << msg.getDestBusId() << "/" << msg.getDestCallId() << endl;
+        if (msg.getDestBusId() == 10 && msg.getDestCallId() == 1)
+            _out1 = msg;
+        else if (msg.getDestBusId() == 10 && msg.getDestCallId() == 2)
+            _out2 = msg;
+        else if (msg.getDestBusId() == 10 && msg.getDestCallId() == 3)
+            _out3 = msg;
+        else 
+            assert(false);
     }
+
+    Message _out1;
+    Message _out2;
+    Message _out3;
 };
 
-int main(int, const char**) {
+bool closeTo(int16_t value, int16_t target) {
+    return (target - 2 < value && value < target + 2);
+}
+
+static void test_1() {
     
     Log log;
     StdClock clock;
@@ -59,23 +72,37 @@ int main(int, const char**) {
         bridge.consume(msg0);
     }
 
-    // Send in some audio
+    // Send in some audio at two different levels
     {
         int16_t audio[960];
         for (unsigned i = 0;i < 960; i++)
             audio[i] = 0.5f * 32767.0f;
-        Message audioIn(Message::Type::AUDIO, CODECType::IAX2_CODEC_SLIN_48K,
+        Message audioIn1(Message::Type::AUDIO, CODECType::IAX2_CODEC_SLIN_48K,
             960 * 2, (const uint8_t*)audio, 0, 0);
-        audioIn.setSource(10, 1);
-        audioIn.setDest(1, 0);
-        bridge.consume(audioIn);
+        audioIn1.setSource(10, 1);
+        audioIn1.setDest(1, 0);
+        bridge.consume(audioIn1);
 
-        audioIn.setSource(10, 2);
-        bridge.consume(audioIn);
+        for (unsigned i = 0;i < 960; i++)
+            audio[i] = 0.25f * 32767.0f;
+        Message audioIn2(Message::Type::AUDIO, CODECType::IAX2_CODEC_SLIN_48K,
+            960 * 2, (const uint8_t*)audio, 0, 0);
+        audioIn2.setSource(10, 2);
+        bridge.consume(audioIn2);
     }
 
     // Tick
     bridge.audioRateTick();
 
-    cout << "hello izzy" << endl;
+    // Make sure the audio got scaled properly
+    int16_t* pcm1 = (int16_t*)sink._out1.body();
+    assert(closeTo(pcm1[0], 0.25f * 32767.0f));
+    int16_t* pcm2 = (int16_t*)sink._out2.body();
+    assert(closeTo(pcm2[0], 0.5f * 32767.0f));
+    int16_t* pcm3 = (int16_t*)sink._out3.body();
+    assert(closeTo(pcm3[0], 0.375f * 32767.0f));
+}
+
+int main(int, const char**) {
+    test_1();
 }
