e3f29687e301570023d6bf11d6f06151590adf8d
brucemack <bruce@mackinnon.com>
2026-01-15 13:02:19 -0500

    Switched to new COBS library

--- src/demos/hello-usb-serial.cpp
+++ src/demos/hello-usb-serial.cpp
@@ -8,12 +8,18 @@
 
 #include <iostream>
 #include <cmath>
+#include <cassert>
 
 #include "kc1fsz-tools/linux/StdClock.h"
 #include "kc1fsz-tools/StdPollTimer.h"
 #include "kc1fsz-tools/Log.h"
 #include "kc1fsz-tools/Common.h"
-#include "kc1fsz-tools/CobsCodec.h"
+#include "cobs.h"
+
+#define NETWORK_BAUD (1152000)
+// Fixed size, 1 header null + audio + 2 COBS overhead
+#define NETWORK_MESSAGE_SIZE (1 + (160 * 2) + 2)
+#define BLOCK_SIZE (160)
 
 using namespace std;
 using namespace kc1fsz;
@@ -34,10 +40,9 @@ int main(int, const char**) {
     // NOTE: This is important! POSIX states that the struct passed to tcsetattr()
     // must have been initialized with a call to tcgetattr() overwise behaviour
     // is undefined
-    if(tcgetattr(serial_port, &tty) != 0) {
+    if (tcgetattr(serial_port, &tty) != 0) {
         printf("Error %i from tcgetattr\n", errno);
     }
-
     tty.c_cflag &= ~PARENB;
     tty.c_cflag &= ~CSTOPB;
     tty.c_cflag &= ~CSIZE; 
@@ -60,65 +65,76 @@ int main(int, const char**) {
     tty.c_cc[VMIN] = 0;
 
     // Specifying a custom baud rate when using GNU C
-    cfsetispeed(&tty, 1152000);
-    cfsetospeed(&tty, 1152000);
+    cfsetispeed(&tty, NETWORK_BAUD);
+    cfsetospeed(&tty, NETWORK_BAUD);
 
     if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
         printf("Error %i from tcsetattr\n", errno);
     }
 
-    // Switch into streaming mode
-    uint8_t packet[135];
-    packet[0] = 'a';
-    int rc0 = write(serial_port, packet, 1);
-    cout << "RC0 = " << rc0 << endl;
-    sleep(1);
-
-    int count = 1;
-
+    uint8_t packet[NETWORK_MESSAGE_SIZE];
     Log log;
     StdClock clock;
-    StdPollTimer timer(clock, 8 * 1000);
-    int recCount = 0;
-    char read_buf[256];
+    StdPollTimer timer(clock, 20 * 1000);
 
+    // Switch into streaming mode
+    packet[0] = 'a';
+    int rc0 = write(serial_port, packet, 1);
+    if (rc0 != 1) {
+        log.error("Failed to send switch command");
+        return -1;
+    }
+    sleep(1);
+
+    int sendCount = 0;
+    int recPtr = 0;
+    char read_buf[256];
     float phi = 0;
     float omega = 2.0f * 3.1415926f * 800.0f / 8000.0f;
 
     while (true) {
 
         if (timer.poll()) {
-            //if (count > 0) 
+
+            if (recPtr > 0) 
+                log.info("Received frame %d", recPtr);
             //    prettyHexDump((const uint8_t*)read_buf, recCount, cout);
 
-            // Make a PCM tone
-            uint8_t msg[128 + 4];
+            // Make a PCM tone in 20ms increments, phase continuous
+            uint8_t msg[BLOCK_SIZE * 2];
             uint8_t* p = msg;
-            for (unsigned i = 0; i < 64; i++, p += 2) {
+            for (unsigned i = 0; i < BLOCK_SIZE; i++, p += 2) {
                 float v = 0.5 * std::cos(phi);
                 phi += omega;
                 int16_t pcm = v * 32767.0f;
                 pack_int16_le(pcm, p);
             }
             phi = fmod(phi, 2.0f * 3.1415926f);
-            // TODO: EXTRA 4 BYTES!
 
-            // Header
-            packet[0] = 0;
-            cobsEncode(msg, 128 + 4, packet + 1, 128 + 4 + 1);
-            int rc1 = write(serial_port, packet, 134);
+            if (sendCount < 100) {
+                // Header
+                packet[0] = 0;
+                cobs_encode_result re = cobs_encode(packet + 1, NETWORK_MESSAGE_SIZE - 1, 
+                    msg, BLOCK_SIZE * 2);
+                assert(re.status == COBS_ENCODE_OK);
+                assert(re.out_len <= NETWORK_MESSAGE_SIZE - 1);
+                int rc1 = write(serial_port, packet, NETWORK_MESSAGE_SIZE);
+                if (rc1 != NETWORK_MESSAGE_SIZE)
+                    log.error("Write failed %d", rc1);
+                sendCount++;
+            }
 
-            recCount = 0;
+            recPtr = 0;
             memset(read_buf, 0, 256);
         }
 
-        int rc2 = read(serial_port, read_buf + recCount, 256);
+        int rc2 = read(serial_port, read_buf + recPtr, 256);
         if (rc2 == 0) {
         }
-        else {
-            recCount += rc2;
-            if (recCount == 0)
-                log.info("   %d", (int)read_buf[0]);
+        else if (rc2 < 0) {
+            log.error("Read error %d", rc2);
+        } else {
+            recPtr += rc2;
         }
     }
 }
