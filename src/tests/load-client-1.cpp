546fc96fe63c48b1aa4f13ed06553646ff863d44
brucemack <bruce@mackinnon.com>
2026-02-11 12:41:04 -0500

    Adding config parameters for load client

--- src/tests/load-client-1.cpp
+++ src/tests/load-client-1.cpp
@@ -23,6 +23,9 @@
 #include <signal.h>
 #include <iostream>
 
+// 3rd party command-line parser
+#include <argparse/argparse.hpp>
+
 // Non-AMP stuff from my C++ tools library
 #include "kc1fsz-tools/Log.h"
 #include "kc1fsz-tools/linux/StdClock.h"
@@ -46,16 +49,67 @@
 using namespace std;
 using namespace kc1fsz;
 
-static const unsigned lineCount = 128;
-static LineIAX2::Call iax2CallSpace[lineCount];
+static const unsigned maxLineCount = 256;
+static LineIAX2::Call iax2CallSpace[maxLineCount];
+static const char* VERSION = "20260211.0";
 
-int main(int, const char**) {
+// EX: ./load-client-1 --count 75 --local 2000
 
+int main(int argc, const char** argv) {
+
+    StdClock clock;
     MTLog2 log;
 
     log.info("AMP Load Client 1");
 
-    StdClock clock;
+    // Parse command line arguments
+    argparse::ArgumentParser program("load-client-1", VERSION);
+   
+    string targetNode = "672731";
+    program.add_argument("--target")
+        .store_into(targetNode)
+        .default_value("672731")
+        .help("Target node");
+
+    program.add_argument("--trace")
+        .help("Turn on network tracing")
+        .default_value(false)
+        .implicit_value(true);
+
+    unsigned count = 0;
+    program.add_argument("--count")
+        .store_into(count)
+        .default_value(128)
+        .help("Client count");
+
+    unsigned localNodeStart = 1000;
+    program.add_argument("--local")
+        .store_into(localNodeStart)
+        .default_value(1000)
+        .help("Starting local node number");
+
+    unsigned ttlSec = 60 * 5;
+    program.add_argument("--ttlsec")
+        .store_into(ttlSec)
+        .default_value(60 * 5)
+        .help("Time-to-live in seconds");
+
+    try {
+        program.parse_args(argc, argv);
+    } catch (const std::exception& err) {
+        log.error("Argument error: %s", err.what());
+        std::exit(-2);
+    }
+
+    if (count > maxLineCount) {
+        cerr << "Maximum lines exceeded." << endl;
+        return -1;
+    }
+
+    log.info("Client count           %u", count);
+    log.info("Target node            %s", targetNode.c_str());
+    log.info("Local node (starting)  %u", localNodeStart);
+    log.info("Time to live (seconds) %u", ttlSec);
 
     // A queue used by other threads to pass messages into the main thread's
     // router.
@@ -70,12 +124,13 @@ int main(int, const char**) {
 
     // This is the Line that makes the IAX2 network connection
     LocalRegistryStd locReg;
-
-    Runnable2* tasks[lineCount + 1];
+    // Will be filled with the tasks to be run by the event loop
+    Runnable2* tasks[maxLineCount + 1];
+    uint64_t startMs = clock.timeMs();
 
     // Make a bunch of lines
-    LineIAX2* lines[lineCount];
-    for (unsigned i = 0; i < lineCount; i++) {
+    LineIAX2* lines[maxLineCount]= { 0 };
+    for (unsigned i = 0; i < count; i++) {
         // Each line only gets one call
         LineIAX2* line = new LineIAX2(log, log, clock, 100 + i, router, 0, 0, &locReg, 10, "radio",
             iax2CallSpace + i, 1);
@@ -88,14 +143,19 @@ int main(int, const char**) {
         }
         // Place a test call
         char localNode[32];
-        snprintf(localNode, sizeof(localNode), "%d", 1000 + i);
+        snprintf(localNode, sizeof(localNode), "%d", localNodeStart + i);
         //line->call(localNode, "radio@127.0.0.1:4569/2000,NONE");
-        line->call(localNode, "672731", CODECType::IAX2_CODEC_G711_ULAW);
+        line->call(localNode, targetNode.c_str(), CODECType::IAX2_CODEC_G711_ULAW);
     }
 
-    int count = 0;
+    TimerTask timer0(log, clock, 10, [&clock, &log, lines, count, startMs, ttlSec]() {
 
-    TimerTask timer0(log, clock, 10, [&clock, lines, &count]() {
+        // Check for shutdown 
+        if (clock.timeMs() - startMs > ttlSec * 1000) {
+            log.info("Shutting down!");
+            exit(0);
+        }
+
         /*
         if (count == 20) {
             cout << "Sending audio" << endl;
@@ -125,12 +185,11 @@ int main(int, const char**) {
             cout << "Time " << (endUs - startUs) << endl;
         }
         */
-        count++;
     });
     tasks[0] = &timer0;
 
     // Setup the EventLoop with all of the tasks that need to be run on this thread
-    EventLoop::run(log, clock, 0, 0, tasks, std::size(tasks), nullptr, false);
+    EventLoop::run(log, clock, 0, 0, tasks, count + 1, nullptr, false);
 
     // #### TODO: At the moment there is no clean way to get out of the loop
 
