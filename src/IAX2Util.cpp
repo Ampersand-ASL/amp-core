91f9efe769ec43f0d0922a52f4c2615c9a336baa
brucemack <bruce@mackinnon.com>
2026-02-22 15:04:24 -0500

    Working on tone test

--- src/IAX2Util.cpp
+++ src/IAX2Util.cpp
@@ -131,7 +131,10 @@ int compareSeqWrap(uint8_t a, uint8_t b) {
  */
 
 uint32_t getSupportedCodecs() { 
-    return CODECType::IAX2_CODEC_G711_ULAW | CODECType::IAX2_CODEC_SLIN_8K | 
+//    return CODECType::IAX2_CODEC_G711_ULAW | CODECType::IAX2_CODEC_SLIN_8K | 
+//        CODECType::IAX2_CODEC_SLIN_16K | CODECType::IAX2_CODEC_G726_AAL2;
+    // #### TODO: NEED TO FIGURE OUT SLIN8
+    return CODECType::IAX2_CODEC_G711_ULAW | 
         CODECType::IAX2_CODEC_SLIN_16K | CODECType::IAX2_CODEC_G726_AAL2;
 }
 
