dc76b123e01d1948754e7d119db33c09d2a37133
brucemack <bruce@mackinnon.com>
2026-02-23 14:14:24 -0500

    Added signal out that can manipulate GPIO3

---
+++ src/SignalOut.cpp
@@ -0,0 +1,96 @@
+/**
+ * Copyright (C) 2026, Bruce MacKinnon KC1FSZ
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <iostream>
+#include <cassert>
+
+#include "kc1fsz-tools/Common.h"
+#include "kc1fsz-tools/Log.h"
+#include "kc1fsz-tools/Clock.h"
+
+#include "MessageConsumer.h"
+#include "SignalOut.h"
+
+using namespace std;
+
+namespace kc1fsz {
+    namespace amp {
+
+SignalOut::SignalOut(Log& log, Clock& clock, MessageConsumer& bus, unsigned radioLineId,
+    Message::SignalType sigTypeOn, Message::SignalType sigTypeOff) 
+:   _log(log), _clock(clock), _bus(bus), _radioLineId(radioLineId), _sigTypeOn(sigTypeOn),
+    _sigTypeOff(sigTypeOff) {
+}
+
+int SignalOut::openHid(const char* hidName) {
+
+    close();
+
+    if ((_hidFd = ::open(hidName, O_RDWR | O_NONBLOCK)) < 0) {
+        _log.error("Cannot open HID device %d", errno);
+        _hidFd = 0;
+        _hidFailed = true;
+        return -1;
+    }
+
+    _hidPacketSize = 4;
+    _hidOffset = 0;
+    _hidMask = 0x04;
+    _hidFailed = false;
+    return 0;
+}
+
+void SignalOut::close() {
+    if (_hidFd)
+        ::close(_hidFd);
+    _hidFd = 0;
+}
+
+// ----- MessageConsumer --------------------------------------------------
+
+void SignalOut::consume(const Message& msg) {
+    if (msg.isSignal(_sigTypeOn)) {
+        // #### TODO: GENERALIZE
+        char msg[5] = { 0 };
+        // GPIO3
+        msg[2] = _hidMask;
+        msg[3] = _hidMask;
+        int rc = write(_hidFd, msg, 5);
+        if (rc != 5)
+            _log.info("Signal out write error %d %d", rc, errno);
+        else
+            _log.info("Signal out PTT on");
+    }
+    else if (msg.isSignal(_sigTypeOff)) {
+        // #### TODO: GENERALIZE
+        char msg[5] = { 0 };
+        // GPIO3
+        msg[2] = _hidMask;
+        msg[3] = 0;
+        int rc = write(_hidFd, msg, 5);
+        if (rc != 5)
+            _log.info("Signal out write error %d %d", rc, errno);
+        else
+            _log.info("Signal out PTT off");
+    }
+}
+    }
+}
---
+++ src/SignalOut.h
@@ -0,0 +1,68 @@
+/**
+ * Copyright (C) 2026, Bruce MacKinnon KC1FSZ
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <cstdint>
+
+#include "Message.h"
+#include "Runnable2.h"
+
+namespace kc1fsz {
+
+class Log;
+class Clock;
+class MessageConsumer;
+
+    namespace amp {
+
+class SignalOut : public Runnable2, public MessageConsumer {
+public:
+
+    SignalOut(Log& log, Clock& clock, MessageConsumer& bus, unsigned radioLineId,
+         Message::SignalType sigTypeOn, Message::SignalType sigTypeOff);
+
+    int openHid(const char* hidName);
+
+    void close();
+
+    // ----- MessageConsumer --------------------------------------------------
+
+    virtual void consume(const Message& frame);
+
+private:
+
+    Log& _log;
+    Clock& _clock;
+    MessageConsumer& _bus;
+    unsigned _radioLineId;
+    Message::SignalType _sigTypeOn;
+    Message::SignalType _sigTypeOff;
+    
+    int _hidFd = 0;
+    bool _hidFailed = false;
+    static const unsigned MAX_HID_SIZE = 16;
+    uint8_t _hidAcc[MAX_HID_SIZE];
+    unsigned _hidPacketSize = 4;
+
+    unsigned _hidOffset = 1;
+    uint8_t _hidMask = 0x04;
+};
+
+    }
+}
+
+   
