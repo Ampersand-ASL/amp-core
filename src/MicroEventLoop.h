0d9626a4c25870e099a04e9d270704c9c791f015
brucemack <bruce@mackinnon.com>
2026-03-19 10:00:54 -0400

    Added new event loop

---
+++ src/MicroEventLoop.h
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2025, Bruce MacKinnon KC1FSZ
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <functional>
+
+namespace kc1fsz {
+
+class Log;
+class Clock;
+class Runnable;
+class Runnable2;
+
+/**
+ * An event loop suitable for a bare-metal/microcontroller context.
+ */
+class MicroEventLoop {
+public:
+
+    /**
+     * @param cb (Optional) Called on every cycle. If false is
+     * returned then the loop exits.
+     * @param cb2 (Optional) Called on every cycle. 
+     */
+    static void run(Log& log, Clock& lock, 
+        Runnable** tasks, unsigned tasksLen,
+        Runnable2** tasks2, unsigned tasks2Len,
+        std::function<bool(Log& log, Clock& clock)> cb = nullptr,
+        void (*cb2)() = 0,
+        bool trace = false);    
+};
+
+}
